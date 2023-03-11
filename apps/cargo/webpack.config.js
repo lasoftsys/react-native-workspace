f0590cc14e08c0924e6b4bb6bafef5600815d136
Rene La <renela79@gmail.com>
2023-03-12 00:56:21 +0200

    nativewind conigs

--- apps/cargo/webpack.config.js
+++ apps/cargo/webpack.config.js
@@ -2,7 +2,15 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const { withNxWebpack } = require('@nrwl/expo');
 
 module.exports = async function (env, argv) {
-  let config = await createExpoWebpackConfigAsync(env, argv);
+  let config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {
+        dangerouslyAddModulePathsToTranspile: ['nativewind'],
+      },
+    },
+    argv
+  );
   config = await withNxWebpack(config);
 
   // You can override the config here, for example:
@@ -10,6 +18,10 @@ module.exports = async function (env, argv) {
   //   ...config.resolve.alias,
   //   react: path.resolve('../../node_modules/react'),
   // };
+  config.module.rules.push({
+    test: /\.css$/i,
+    use: ['postcss-loader'],
+  });
 
   return config;
 };
