5a19e34426aff383929b18434e66b27da27fae52
Rene La <renela79@gmail.com>
2023-06-02 00:35:00 +0200

    app config

--- apps/cargo/src/app/App.tsx
+++ apps/cargo/src/app/App.tsx
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{useCallback } from 'react';
-import { Text, View, LogBox } from 'react-native';
-import { styled } from 'nativewind';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import {SafeAreaProvider} from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar';
+import React, { useCallback } from 'react';
+import { LogBox } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from '../navigation/root.navigator';
 
 LogBox.ignoreAllLogs(true);
@@ -12,16 +11,15 @@ SplashScreen.preventAutoHideAsync();
 
 export const App = () => {
   const [fontsLoaded] = useFonts({
-    "Nunito-Black": require('../../assets/fonts/nunito/Nunito-Black.ttf'),
-    "Nunito-Bold": require('../../assets/fonts/nunito/Nunito-Bold.ttf'),
-    "Nunito-ExtraBold": require('../../assets/fonts/nunito/Nunito-ExtraBold.ttf'),
-    "Nunito-ExtraLight": require('../../assets/fonts/nunito/Nunito-ExtraLight.ttf'),
-    "Nunito-Light": require('../../assets/fonts/nunito/Nunito-Light.ttf'),
-    "Nunito-Medium": require('../../assets/fonts/nunito/Nunito-Medium.ttf'),
-    "Nunito-Regular": require('../../assets/fonts/nunito/Nunito-Regular.ttf'),
-    "Nunito-SemiBold": require('../../assets/fonts/nunito/Nunito-SemiBold.ttf'),
+    'Nunito-Black': require('../../assets/fonts/nunito/Nunito-Black.ttf'),
+    'Nunito-Bold': require('../../assets/fonts/nunito/Nunito-Bold.ttf'),
+    'Nunito-ExtraBold': require('../../assets/fonts/nunito/Nunito-ExtraBold.ttf'),
+    'Nunito-ExtraLight': require('../../assets/fonts/nunito/Nunito-ExtraLight.ttf'),
+    'Nunito-Light': require('../../assets/fonts/nunito/Nunito-Light.ttf'),
+    'Nunito-Medium': require('../../assets/fonts/nunito/Nunito-Medium.ttf'),
+    'Nunito-Regular': require('../../assets/fonts/nunito/Nunito-Regular.ttf'),
+    'Nunito-SemiBold': require('../../assets/fonts/nunito/Nunito-SemiBold.ttf'),
   });
-
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
@@ -35,7 +33,7 @@ export const App = () => {
   return (
     <SafeAreaProvider onLayout={onLayoutRootView}>
       <StatusBar style="auto" />
-      <RootNavigator/>
+      <RootNavigator />
     </SafeAreaProvider>
   );
 };
