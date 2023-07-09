3a78c94c740e6427d25fd81f872f24e072a846c8
Rene La <renela@Rene-2.local>
2023-07-09 09:33:20 +0200

    components lib:cargo

---
+++ libs/cargo/components/src/lib/country-code/CountryCode.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+/* eslint-disable-next-line */
+export interface StyleProps { }
+
+const Wrapper = styled.div`
+	color: pink;
+`;
+
+
+export { Wrapper };
---
+++ libs/cargo/components/src/lib/country-code/CountryCode.tsx
@@ -0,0 +1,17 @@
+
+import { Wrapper } from "./CountryCode.styled";
+/* eslint-disable-next-line */
+export interface CountryCodeProps { }
+
+
+export function CountryCode(
+	props: CountryCodeProps
+) {
+	return (
+		<Wrapper>
+			<h1>Welcome to CountryCode!</h1>
+		</Wrapper>
+	);
+}
+
+export default CountryCode;
